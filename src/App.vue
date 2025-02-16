f8178a982dd90360bcca62b809197e19dd50d816
Jonathan0890 <jonathan.coox15@gmail.com>
2025-02-16 01:44:09 -0500

    Completado

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,11 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-import MainNav from './components/MainNav.vue'
-//import MFooter from './components/MFooter.vue';
 
 
 </script>
 
 <template>
   <header>
-    <MainNav />
   </header>
   <RouterView />
 </template>
